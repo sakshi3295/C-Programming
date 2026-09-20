280b0a3de3e33cbbaaecaeb886c0f226b7cb9721
Sakshi Nalawade <snalawade373@gmail.com>
2026-09-20 10:49:41 +0530

    Add files via upload

---
+++ Assignment No 1/18 printf Formated Output.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+#include<conio.h>
+
+int main()
+{
+    printf("\n\t Student information Database \n");
+
+    printf("\n------------------******-------------------\n");
+
+    printf("\n %4d %10s %3d %3d %3d %3d %0.2f",107,"sakshi",6,95,65,166,55.33333);
+    printf("\n %4d %10s %3d %3d %3d %3d %0.2f",8,"Shraddha",65,85,50,200,66.66666);
+    printf("\n %4d %10s %3d %3d %3d %3d %0.2f",40,"Harshada",55,45,80,180,60.00000);
+
+    printf("\n\-----------------******--------------------\n");
+
+
+    getch();
+    return 0;
+}
