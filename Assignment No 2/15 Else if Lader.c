8a3897a86d00a65f3c07473dc8bfb03c16e3e5b4
Sakshi Nalawade <snalawade373@gmail.com>
2026-09-20 11:31:16 +0530

    Add files via upload

---
+++ Assignment No 2/15 Else if Lader.c
@@ -0,0 +1,30 @@
+#include<stdio.h>
+#include<conio.h>
+
+int main()
+{
+    char ch =0;
+
+    printf("\n Enter the character = ");
+    scanf("%c",&ch);
+
+        if(ch>='A'&&ch<='Z')
+        {
+            printf("\n The character %c is Upper Case",ch);
+        }
+        else if(ch>='a'&&ch<='z')
+        {
+            printf("\n The character %c is Lower Case",ch);
+        }
+        else if(ch>='0'&&ch<'9')
+        {
+            printf("\n The character %c is Digit",ch);
+        }
+        else
+        {
+            printf("\n The character %c is Special symbol",ch);
+        }
+        getch();
+        return 0;
+
+}
