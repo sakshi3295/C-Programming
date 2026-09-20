afba48acf347772ea8f6356c754a129f7f83287b
Sakshi Nalawade <snalawade373@gmail.com>
2026-09-20 10:34:11 +0530

    Add files via upload

---
+++ Assignment No 1/02 Addition of two Numbers.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+#include<conio.h>
+
+int main()
+{
+    int No1=0,No2=0,Result=0;
+
+    printf("\n Enter A Number1 :");
+    scanf("%d",&No1);
+    printf("\n Enter A Number2 :");
+    scanf("%d",&No2);
+
+    Result=No1+No2;
+    {
+        printf("\n The result of adding %d and %d is: %d ",No1,No2,Result);
+    }
+     getch();
+     return 0;
+
+}
