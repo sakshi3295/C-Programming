d34d7d19ba757d0cd1226a5142e930524bae2749
Sakshi Nalawade <snalawade373@gmail.com>
2026-09-20 11:26:03 +0530

    Add files via upload

---
+++ Assignment No 2/05 Divisible By 7.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+#include<conio.h>
+
+int main()
+{
+    int No=0;
+
+    printf("\n Enter a Number = ");
+    scanf("%d",&No);
+
+    if(No % 7 == 0)
+    {
+        printf("\n %d is Divisible By 7 ",No);
+    }
+    else
+    {
+        printf("\n %d is Not Divisible By 7 ",No);
+    }
+    getch();
+    return 0;
+}
