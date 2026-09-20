369edf42141928b7e0682607c0046da22bf2b512
Sakshi Nalawade <snalawade373@gmail.com>
2026-09-20 11:27:58 +0530

    Add files via upload

---
+++ Assignment No 2/09 Month Name.c
@@ -0,0 +1,65 @@
+#include<stdio.h>
+#include<conio.h>
+
+int main()
+{
+    int No=0;
+
+    printf("\n Enter a Month Number =  ");
+    scanf("%d",&No);
+
+    if(No == 1)
+    {
+        printf("\n The Month is = January");
+    }
+    else if(No == 2)
+    {
+        printf("\n The Month is = February");
+    }
+     else if(No == 3)
+    {
+        printf("\n The Month is = March");
+    }
+     else if(No == 4)
+    {
+        printf("\n The Month is = April");
+    }
+     else if(No == 5)
+    {
+        printf("\n The Month is = May");
+    }
+     else if(No == 6)
+    {
+        printf("\n The Month is = June");
+    }
+     else if(No == 7)
+    {
+        printf("\n The Month is = July");
+    }
+     else if(No == 8)
+    {
+        printf("\n The Month is = August");
+    }
+     else if(No == 9)
+    {
+        printf("\n The Month is = September");
+    }
+     else if(No == 10)
+    {
+        printf("\n The Month is = October");
+    }
+     else if(No == 11)
+    {
+        printf("\n The Month is = November");
+    }
+     else if(No == 12)
+    {
+        printf("\n The Month is = December");
+    }
+    else
+    {
+        printf("\n Invalid Month Number");
+    }
+    getch();
+    return 0;
+}
