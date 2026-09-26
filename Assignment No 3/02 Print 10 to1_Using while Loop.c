353384e1fba5e99c091319b70c03e0b90e86c746
Sakshi Nalawade <snalawade373@gmail.com>
2026-09-26 23:42:00 +0530

    Add files via upload

---
+++ Assignment No 3/02 Print 10 to1_Using while Loop.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+#include<conio.h>
+
+int main()
+{
+    int No = 10;
+
+    while(No >= 1)
+    {
+        printf("\n %d",No);
+        No--;
+    }
+        printf("\n\n End\n");
+        getch();
+        return 0;
+}
---
+++ Assignment No 3/02B Print 10 to 1_Using For Loop.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+#include<conio.h>
+
+int main()
+{
+    int No = 0;
+
+    for(No = 10; No >= 1; No--)
+    {
+        printf("\n %d",No);
+    }
+    printf("\n\n End \n");
+    getch();
+    return 0;
+}
