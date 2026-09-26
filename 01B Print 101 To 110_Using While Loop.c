2794515d5bed93ea5f476f0fd9d109c875194906
Sakshi Nalawade <snalawade373@gmail.com>
2026-09-26 23:35:28 +0530

    Add files via upload

---
+++ 01B Print 101 To 110_Using While Loop.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+#include<conio.h>
+
+int main()
+{
+    int No = 101;
+
+    while(No <= 110)
+    {
+
+        printf("\n %d",No);
+        No++;
+    }
+        printf("\n\n End \n");
+
+        getch();
+        return 0;
+}
